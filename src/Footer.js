66b867488b0f49e3e2f90312257bc66e7e8261ed
Nitish pal <nitishpal99@gmail.com>
2023-02-26 16:13:58 +0000

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <nav class="navbar navbar-light bg-light d-flex p-5 d-flex justify-content-center">
+      <nav class="navbar navbar-light bg-light d-flex p-5 d-flex justify-content-center align-self-end ">
         <a class="navbar-brand center" href="#">
           Developed By Nitish &copy; 2023
         </a>
